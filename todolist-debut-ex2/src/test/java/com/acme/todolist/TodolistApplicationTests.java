df64aca7c3f1a67d57aa126af8a8955a51d01841
alexandre <alexandre.ruaux@sfr.fr>
2023-04-04 11:02:40 +0200

    exo 3 fin

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,11 +1,11 @@
 package com.acme.todolist;
 
-import static org.junit.Assert.assertEquals;
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
-import org.junit.Test;
+import com.acme.todolist.domain.TodoItem;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TodolistApplicationTests {
 
@@ -17,11 +17,9 @@ class TodolistApplicationTests {
 		// Item en retard
 		TodoItem lateItem = new TodoItem("1", Instant.now().minus(2, ChronoUnit.DAYS), "Item en retard");
 		assertEquals("[LATE!] Item en retard", lateItem.finalContent());
-
 		// Item en avance
 		TodoItem earlyItem = new TodoItem("2", Instant.now().plus(2, ChronoUnit.DAYS), "Item en avance");
 		assertEquals("Item en avance", earlyItem.finalContent());
-
 		// Item à l'heure (créé juste avant la vérification)
 		TodoItem onTimeItem = new TodoItem("3", Instant.now(), "Item à l'heure");
 		assertEquals("Item à l'heure", onTimeItem.finalContent());
